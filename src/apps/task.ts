34a9a8684fc4e670091d0e38bf4cc2f1410724a8
时瑾 <74231782+shijinn520@users.noreply.github.com>
2024-11-29 02:48:09 +0800

    refactor: 重构文件结构

--- src/apps/task.ts
+++ src/apps/task.ts
@@ -6,6 +6,6 @@ import { karin, logger } from 'node-karin'
  * 参数2: cron表达式
  * 参数3: 任务方法
  */
-export const Task = karin.task('1分钟打印1次hello', '0 */1 * * * *', async () => {
+export const Task = karin.task('1分钟打印1次hello', '0 */1 * * * *', () => {
   logger.info('hello')
-})
+}, { log: false })
